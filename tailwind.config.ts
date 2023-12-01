0ebe8b6bc32c5cd7a7e555236d83cb933146fec2
tuananh2606 <ngdtuananh01@gmail.com>
2023-12-01 21:10:06 +0700

    Update Preview Modal

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,41 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        'scale-in-center': 'scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both',
+        'scale-up-center': 'scale-up-center 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both',
+        'scale-out-center': 'scale-out-center 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530)   both',
+      },
+      keyframes: {
+        'scale-up-center': {
+          '0%': {
+            transform: 'scale(.5)',
+          },
+          to: {
+            transform: 'scale(1)',
+          },
+        },
+        'scale-in-center': {
+          '0%': {
+            transform: 'scale(0)',
+            opacity: '1',
+          },
+          to: {
+            transform: 'scale(1)',
+            opacity: '1',
+          },
+        },
+        'scale-out-center': {
+          '0%': {
+            transform: 'scale(1)',
+            opacity: '1',
+          },
+          to: {
+            transform: 'scale(0)',
+            opacity: '1',
+          },
+        },
+      },
     },
   },
   plugins: [],
