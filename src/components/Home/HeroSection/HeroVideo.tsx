a6b37a80aed90551226ba2f97018eb9b5e8b1c09
Nguyễn Đình Tuấn Anh <ngdtuananh01@gmail.com>
2024-02-26 14:33:06 +0700

    refactor: utils, herov ideo

--- src/components/Home/HeroSection/HeroVideo.tsx
+++ src/components/Home/HeroSection/HeroVideo.tsx
@@ -1,13 +1,13 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import { IAnimeInfo, IMovieInfo } from '@consumet/extensions';
 
-import HeroPlayer from '../player/HeroPlayer';
+import HeroPlayer from '../../player/HeroPlayer';
 import styles from './hero-video.module.scss';
 import { IVideos, ICredit, IMovieDetail, ITvShowDetail } from '~/services/tmdb/tmdb.types';
-import { SelectProvidersDialog, PreviewModal } from '../dialog';
+import { SelectProvidersDialog, PreviewModal } from '../../dialog';
 import { IMedia } from '~/types/media';
-import { Dialog, DialogContent, DialogTrigger } from '../elements/Dialog';
+import { Dialog, DialogContent, DialogTrigger } from '../../elements/Dialog';
 import { TMDB } from '~/services/tmdb/utils.server';
 
 interface IHeroVideo {
@@ -61,14 +61,10 @@ const HeroVideo = ({ items }: IHeroVideo) => {
   };
 
   // preload(items?.id as number, items?.mediaType);
-
   return (
-    <div className="relative h-[56.25vw] max-h-[100dvh]">
+    <div className="relative">
       <HeroPlayer isMuted={isMuted} playing={isPlaying} url={trailerVideo?.key} backdropPath={backdropPath} />
-      {isOpenModal && (
-        <div className="previewMpdel-backdrop fixed inset-0 z-30 h-full w-full bg-black opacity-70"></div>
-      )}
-      <div className="absolute bottom-[10%] left-[5%] flex w-[60%] flex-col justify-end md:w-[36%]">
+      <div className="absolute bottom-[20%] left-[5%] flex w-[60%] flex-col justify-end md:w-[36%]">
         <h1 className=" text-base drop-shadow-xl md:text-2xl">{title}</h1>
         <div className="mt-[1.5vw] animate-fade-bottom text-sm drop-shadow-xl md:text-lg">
           <div className="overflow-hidden">{details?.overview}</div>
@@ -79,7 +75,12 @@ const HeroVideo = ({ items }: IHeroVideo) => {
               <DialogTrigger asChild>
                 <button className="mr-3 flex items-center justify-center rounded-[4px] bg-white px-2 py-2 transition hover:bg-[rgba(255,255,255,0.75)] md:px-7">
                   <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none">
+                    <svg
+                      className="h-6 w-6 md:h-8 md:w-8"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
                       <path
                         d="M19.376 12.4158L8.77735 19.4816C8.54759 19.6348 8.23715 19.5727 8.08397 19.3429C8.02922 19.2608 8 19.1643 8 19.0656V4.93408C8 4.65794 8.22386 4.43408 8.5 4.43408C8.59871 4.43408 8.69522 4.4633 8.77735 4.51806L19.376 11.5838C19.6057 11.737 19.6678 12.0474 19.5146 12.2772C19.478 12.3321 19.4309 12.3792 19.376 12.4158Z"
                         fill="black"
@@ -112,7 +113,7 @@ const HeroVideo = ({ items }: IHeroVideo) => {
             className="flex min-w-fit items-center justify-center rounded-[4px] bg-secondary px-2 py-2 transition hover:bg-[rgba(109,109,110,0.4)] md:px-7 "
           >
             <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 25" fill="none">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 md:h-8 md:w-8" viewBox="0 0 24 25" fill="none">
                 <path
                   d="M12 22.3137C6.47715 22.3137 2 17.8365 2 12.3137C2 6.79087 6.47715 2.31372 12 2.31372C17.5228 2.31372 22 6.79087 22 12.3137C22 17.8365 17.5228 22.3137 12 22.3137ZM12 20.3137C16.4183 20.3137 20 16.732 20 12.3137C20 7.89544 16.4183 4.31372 12 4.31372C7.58172 4.31372 4 7.89544 4 12.3137C4 16.732 7.58172 20.3137 12 20.3137ZM11 7.31372H13V9.31372H11V7.31372ZM11 11.3137H13V17.3137H11V11.3137Z"
                   fill="white"
@@ -124,7 +125,7 @@ const HeroVideo = ({ items }: IHeroVideo) => {
         </div>
       </div>
 
-      <div className="absolute bottom-[10%] right-[5%]">
+      <div className="absolute bottom-[20%] right-[5%]">
         <button className="rounded-full border p-2" onClick={muteHandler}>
           {isMuted ? (
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none">
@@ -144,13 +145,11 @@ const HeroVideo = ({ items }: IHeroVideo) => {
         </button>
       </div>
       {isOpenModal && (
-        <div className="absolute left-0 top-0 z-40 flex h-full w-full justify-center">
-          <PreviewModal
-            items={{ credits: credits, details: details }}
-            setIsOpenModal={setIsOpenModal}
-            setIsPlaying={setIsPlaying}
-          />
-        </div>
+        <PreviewModal
+          items={{ credits: credits, details: details }}
+          setIsOpenModal={setIsOpenModal}
+          setIsPlaying={setIsPlaying}
+        />
       )}
     </div>
   );
