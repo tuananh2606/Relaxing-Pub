83f92a2a09f4b4f93d837db6282318ba0d47e26b
Nguyễn Đình Tuấn Anh <ngdtuananh01@gmail.com>
2024-02-29 18:50:33 +0700

    style: header, footer, carousel, navbar

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,14 +1,14 @@
 import styles from './header.module.scss';
-import NavBar from '~/components/ui/navbar/NavBar';
+import { NavBar } from '~/layouts';
 
 const Header = () => {
   return (
     <header>
-      <div
-        className={` ${styles.mainHeader} fixed left-0 top-0 z-20 flex h-16 max-h-[70px] w-full items-center bg-header-bg px-[4%] lg:h-16 lg:bg-transparent`}
+      <nav
+        className={` ${styles.mainHeader} fixed left-0 top-0 z-20 flex h-16 max-h-[70px] w-full items-center bg-transparent px-[4%] lg:h-16`}
       >
         <NavBar />
-      </div>
+      </nav>
     </header>
   );
 };
