6ed70c7f2507c892cd4cb59441a8561838bac4b6
Nguyễn Đình Tuấn Anh <ngdtuananh01@gmail.com>
2024-04-17 22:59:50 +0700

    style: add some style

--- src/components/ui/Skeleton/index.tsx
+++ src/components/ui/Skeleton/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+import AspectRatio from '~/components/shared/AspectRatio';
 import { cn } from '~/lib/utils';
 const shimmer =
   'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent';
@@ -5,7 +7,7 @@ const shimmer =
 export function CardSkeleton({ className }: { className?: string }) {
   return (
     <div
-      className={cn(`${shimmer} relative h-80 w-52 overflow-hidden rounded-xl  bg-gray-500/80 shadow-sm`, className)}
+      className={cn(`${shimmer} relative h-80 w-auto overflow-hidden rounded-xl  bg-gray-500/80 shadow-sm`, className)}
     >
       <div className="h-full w-full  bg-[rgba(255,255,255,0.2)]" />
     </div>
@@ -14,14 +16,16 @@ export function CardSkeleton({ className }: { className?: string }) {
 
 export const HomePageSkeleton = () => {
   return (
-    <div className="mt-16">
+    <div className="mt-16 h-screen w-full px-2">
       <div
         className="relative flex h-[56.25vw] w-full flex-col items-center justify-center overflow-hidden bg-gray-500/80 p-4
-      before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_1.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/10 before:to-transparent md:min-h-screen"
+      before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_1.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/10 before:to-transparent"
       ></div>
-      <div className="mt-4 flex justify-between  md:hidden">
+      <div className="mt-4 grid grid-cols-2 gap-2 md:grid-cols-4">
         <CardSkeleton />
-        <CardSkeleton className="ml-1" />
+        <CardSkeleton />
+        <CardSkeleton className="hidden md:block" />
+        <CardSkeleton className="hidden md:block" />
       </div>
     </div>
   );
@@ -42,5 +46,13 @@ export const VideoSkeleton = () => {
         </svg>
       </div>
     </div>
+  );
+};
+export const HeroSkeleton = () => {
+  return (
+    <div
+      className="relative flex h-[56.25vw] w-full flex-col items-center justify-center overflow-hidden bg-gray-500/80 p-4
+      before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_1.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/10 before:to-transparent"
+    ></div>
   );
 };
